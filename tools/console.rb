0504fad3f91ad9f5c6a884f7a64385ec1682aaec
github username <github email address>
2020-04-03 11:54:25 -0400

    Brandon coding\!

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,12 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+cult1 = Cult.new("The Shadows", "Atlanta", 2020, "It's dark in here")
+cult2 = Cult.new("The Anti-Shadows", "New York", 1984, "You shall not pass")
+cult3 = Cult.new("Flatiron School", "Everywhere", 2010, "I hope I pass")
+cult4 = Cult.new("Facebook Anonymous", "Everywhere", 2010, "We type good")
+cult5 = Cult.new("Corona Virus", "Everywhere", 2019, "Stay inside")
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
